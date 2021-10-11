249112ef5f0b93f3cd593e955098e70f9b1feee3
sanjay <sanjay.n@finlabsindia.com>
2021-10-11 19:35:03 +0530

    logo page

--- common/js/min.js
+++ common/js/min.js
@@ -1,10 +1,2 @@
 
-
-function login() {
-    if ($("#username").val() == "1" && $("#password").val() == "1") {
-        window.location.href = '../../imgcopper/index.html'; //Will take you to Google.
-    } else {
-      alert("You are not a valid user");
-    }
-  }
   
