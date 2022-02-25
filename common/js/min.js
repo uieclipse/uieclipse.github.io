a717850713f03e1a3d6f0ac697e04d12f5883ef9
sanjaynandi13 <sanjaynandi13@gmail.com>
2022-02-26 00:35:00 +0530

    lo

---
+++ common/js/min.js
@@ -0,0 +1,7 @@
+function login() {
+    if ($("#username").val() == "1" && $("#password").val() == "1") {
+        window.location.href = '../../src/dashbord/dashbord.html'; //Will take you to Google.
+    } else {
+        alert("You are not a valid user");
+    }
+} 
